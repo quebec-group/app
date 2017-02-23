652d96fcd9453bf0d45c24260612c5f0ed20d210
Miruna Damian <mmd35@cam.ac.uk>
2017-02-23 14:22:34 +0000

    Added video scrubbing, a save button for event details and a test theme

--- app/src/main/java/com/quebec/app/EventAddDetailsActivity.java
+++ app/src/main/java/com/quebec/app/EventAddDetailsActivity.java
@@ -3,6 +3,7 @@ package com.quebec.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.quebec.app.R;
 
@@ -12,6 +13,11 @@ public class EventAddDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details_adding);
+    }
+
+    public void saveDetails(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
