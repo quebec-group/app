e24020cafe80ebd702a497681c7e28ce2626b752
Callum Ryan <cpr41@cam.ac.uk>
2017-02-28 17:53:30 +0000

    Best guess cognito ID

--- app/src/main/java/com/quebec/services/AWSWrapper.java
+++ app/src/main/java/com/quebec/services/AWSWrapper.java
@@ -1,17 +1,33 @@
 package com.quebec.services;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+
 /**
  * Created by callum on 28/02/2017.
  */
 
 public class AWSWrapper {
+    private static String LOG_TAG = AWSWrapper.class.getSimpleName();
     private static String cognitoID;
+    private static CognitoCachingCredentialsProvider credentialsProvider;
 
     public static String getCognitoID() {
+        if (cognitoID != null) {
+            return cognitoID;
+        }
+
+        if (credentialsProvider != null) {
+            setCognitoID(credentialsProvider.getIdentityId());
+        }
+
         return cognitoID;
     }
 
     public static void setCognitoID(String cognitoID) {
         AWSWrapper.cognitoID = cognitoID;
+    }
+
+    public static void setCredentialsProvider(CognitoCachingCredentialsProvider credentialsProvider) {
+        AWSWrapper.credentialsProvider = credentialsProvider;
     }
 }
