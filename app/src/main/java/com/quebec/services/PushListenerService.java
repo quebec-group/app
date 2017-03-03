b3c28536a81d7355daff996b46ce848b8482ad6b
Callum Ryan <cpr41@cam.ac.uk>
2017-03-03 01:02:05 +0000

    Notifications!

--- app/src/main/java/com/quebec/services/PushListenerService.java
+++ app/src/main/java/com/quebec/services/PushListenerService.java
@@ -1,8 +1,25 @@
 package com.quebec.services;
 
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.gcm.GcmListenerService;
+import com.quebec.app.MainActivity;
+import com.quebec.app.R;
+
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE;
+import static com.amazonaws.mobile.util.ThreadUtils.runOnUiThread;
 
 /** A service that listens to GCM notifications. */
 public class PushListenerService extends GcmListenerService {
@@ -31,9 +48,46 @@ public class PushListenerService extends GcmListenerService {
      */
     @Override
     public void onMessageReceived(final String from, final Bundle data) {
-        String message = getMessage(data);
+        final String message = getMessage(data);
         Log.d(LOG_TAG, "From: " + from);
         Log.d(LOG_TAG, "Message: " + message);
 
+
+        if (isAppOpened()) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            sendNotification(message);
+        }
+    }
+
+    public boolean isAppOpened() {
+        ActivityManager.RunningAppProcessInfo appProcessInfo = new ActivityManager.RunningAppProcessInfo();
+        ActivityManager.getMyMemoryState(appProcessInfo);
+        return (appProcessInfo.importance == IMPORTANCE_FOREGROUND || appProcessInfo.importance == IMPORTANCE_VISIBLE);
+    }
+
+    private void sendNotification(String message) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_logo)
+                .setContentTitle(message)
+                .setContentText("")
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+        Log.d(LOG_TAG, "Sending notification");
     }
 }
