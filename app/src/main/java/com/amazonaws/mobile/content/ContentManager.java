d15f8db32b880f143168835f4ee4a4018c4cae39
Callum Ryan <callum@MacBook-Pro-9.local>
2017-03-08 12:59:25 +0000

    Bug fixes

--- app/src/main/java/com/amazonaws/mobile/content/ContentManager.java
+++ app/src/main/java/com/amazonaws/mobile/content/ContentManager.java
@@ -20,6 +20,7 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.quebec.app.Monitor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -598,6 +599,8 @@ public class ContentManager implements Iterable<ContentItem> {
                     }
                 }
                 // Begin downloading content.
+                Monitor.getInstance().s3Call("Getting file ");
+
                 transferHelper.download(filePath, fileSize, listener);
             }
         });
